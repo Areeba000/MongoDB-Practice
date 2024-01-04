06334cb894424ecb51b0b442703b93dc9f67f081
Areeba000 <areebarafiq567@gmail.com>
2024-01-04 18:07:25 +0500

    newfile

--- ERC20contract/app.js
+++ ERC20contract/app.js
@@ -1,12 +1,9 @@
 const { ethers } = require('ethers');
 require('dotenv').config();
-const mongoose=require(`mongoose`);
-const dbURI="mongodb+srv://areeba1234:areeba1234@cluster0.cmss8dd.mongodb.net/"
-mongoose.connect(dbURI)
-.then(() => console.log('Connected to MongoDB'))
-.catch(error => console.error('Error connecting to MongoDB:', error));
-const Blog2=require(`./erc20events.js`);
+const { MongoClient } = require("mongodb");
 
+const dbURI="mongodb+srv://areeba1234:areeba1234@cluster0.cmss8dd.mongodb.net/"
+const client = new MongoClient(dbURI)
 
 const API_URL = process.env.API_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
@@ -20,25 +17,25 @@ const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 const  abi = require('./contracts/erc20.json');
 const contract = new ethers.Contract(contractADDRESS, abi, signer);
 
-
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+
+async function mongoConnection(){
+await client.connect()
+
+}
 
 app.post('/mint', async (req, res) => {
     const { account, amount } = req.body;
 
     try {
         const transaction = await contract.mint(account, amount);
-       
+        const db=client.db("new");
+        const collection=db.collection("collection3");
+       const result= await collection.insertOne(transaction);
         console.log(transaction)
-        const blog = new Blog2({
-            transactionHash:transaction.hash,
-            // blockNumber:transaction.blockNumber,
-            // amount:transaction.amount,
-            Address:transaction.to
-        });
-        await blog.save()
         res.json({ success: true, transactionHash: transaction.transactionHash });
     } catch (error) {
         console.error(error);
@@ -49,8 +46,12 @@ app.post('/burn', async (req, res) => {
     const { account, amount } = req.body;
 
     try {
-        const result = await contract.burn(account, amount)
-        res.json({ success: true, transactionHash: result.transactionHash });
+        const transaction = await contract.burn(account, amount)
+       const db=client.db("new");
+       const collection=db.collection("collection3");
+       await collection.insertOne(transaction);
+
+        res.json({ success: true, transactionHash: transaction.transactionHash });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, error: error.message });
@@ -60,13 +61,9 @@ app.post('/getbalance', async (req, res) => {
     const { ownerAddress } = req.body;
 
     try {
-        // Call the getbalanceOf function on the smart contract
-        const balance = await contract.balanceOf(ownerAddress);
-
-        // Convert the BigInt balance to a string
-        const balanceString = balance.toString();
-
-        res.json({ success: true, balance: balanceString });
+        const transaction = await contract.balanceOf(ownerAddress);
+         const balanceString = transaction.toString();
+         res.json({ success: true, balance: balanceString });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, error: error.message });
@@ -78,7 +75,9 @@ app.post('/transfer', async (req, res) => {
     try {
       
         const result = await contract.transfer(recipient, amount)
-
+        const db=client.db("new");
+        const collection=db.collection("collection3");
+        await collection.insertOne(result); 
         res.json({ success: true, transactionHash: result.transactionHash });
     } catch (error) {
         console.error(error);
@@ -90,7 +89,9 @@ app.post('/approve', async (req, res) => {
 
     try {
         const result = await contract.approve(spender, amount);
-
+        const db=client.db("new");
+        const collection=db.collection("collection3");
+        await collection.insertOne(result); 
         res.json({ success: true, transactionHash: result.transactionHash });
     } catch (error) {
         console.error(error);
@@ -101,10 +102,7 @@ app.post('/allowance', async (req, res) => {
     const { owner, spender } = req.body;
 
     try {
-        // Call the allowancefun function on the smart contract
         const allowance = await contract.allowancefun(owner, spender);
-
-        // Convert the BigInt allowance to a string
         const allowanceString = allowance.toString();
 
         res.json({ success: true, allowance: allowanceString });
@@ -119,16 +117,25 @@ app.post('/transferFrom', async (req, res) => {
     try {
      
         const result = await contract.transferFrom(sender, recipient, amount);
-
+        const db=client.db("new");
+        const collection=db.collection("collection3");
+        await collection.insertOne(result); 
         res.json({ success: true, transactionHash: result.transactionHash });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, error: error.message });
     }
 });
- 
-
+async function main(){
+    const db=client.db("new");
+    const collection=db.collection("collection3");
+    const result = await collection.findOne({ nonce: 215});
+    console.log(result);
+}
 const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+main();
+mongoConnection()
+  
